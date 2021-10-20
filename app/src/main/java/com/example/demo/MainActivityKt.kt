40e3d48a6d6aec6331f7d565b0e7842de96237e1
Bhumit Belani <bhumit.belani@hiddenbrains.in>
2021-10-20 13:42:11 +0530

    Request methods

--- app/src/main/java/com/example/demo/MainActivityKt.kt
+++ app/src/main/java/com/example/demo/MainActivityKt.kt
@@ -10,36 +10,44 @@ import com.master.permissionhelper.PermissionHelper
 class MainActivityKt : AppCompatActivity() {
     private val TAG = "MainActivity"
 
-    internal var permissionHelper: PermissionHelper? = null
-
+    private val permissionHelper by lazy {
+        PermissionHelper(
+            this,
+            arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CALL_PHONE))
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        permissionHelper = PermissionHelper(this, arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CALL_PHONE), 100)
 
-        //Short Way
-        permissionHelper?.denied {
-            if (it) {
-                Log.d(TAG, "onPermissionDeniedBySystem() called")
-                permissionHelper?.openAppDetailsActivity()
-            } else {
-                Log.d(TAG, "onPermissionDenied() called")
-            }
-        }
+        permissionHelper.requestIndividual(
+            requestIndividualCallback = {
+                Log.d(TAG, "Individual Permission Granted")
+            }, deniedCallback = {isDeniedBySystem->
+                if (isDeniedBySystem) {
+                    Log.d(TAG, "onPermissionDeniedBySystem() called")
+                    permissionHelper.openAppDetailsActivity()
+                } else {
+                    Log.d(TAG, "onPermissionDenied() called")
+                }
+            })
 
-        permissionHelper?.requestIndividual {
-            Log.d(TAG, "Individual Permission Granted")
-        }
-
-        permissionHelper?.requestAll {
-            Log.d(TAG, "All permission granted")
-        }
-
+        permissionHelper.requestAll(
+            requestAllCallback = {
+                Log.d(TAG, "All Permission Granted")
+            },
+            deniedCallback = {isDeniedBySystem->
+                if (isDeniedBySystem) {
+                    Log.d(TAG, "onPermissionDeniedBySystem() called")
+                    permissionHelper.openAppDetailsActivity()
+                } else {
+                    Log.d(TAG, "onPermissionDenied() called")
+                }
+            })
 
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        permissionHelper?.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        permissionHelper.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 }
--- permissionhelper/src/main/java/com/master/permissionhelper/PermissionHelper.kt
+++ permissionhelper/src/main/java/com/master/permissionhelper/PermissionHelper.kt
@@ -1,31 +1,29 @@
 package com.master.permissionhelper
 
 import android.app.Activity
-import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.provider.Settings
-import androidx.core.app.ActivityCompat
-import androidx.fragment.app.Fragment
-import androidx.core.content.ContextCompat
 import android.util.Log
-import java.util.ArrayList
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import java.util.*
 
 class PermissionHelper {
 
     private val TAG = "PermissionHelperJava"
-    private val permissionHelper: PermissionHelperJava? = null
     private var REQUEST_CODE: Int = 0
 
-    private var activity: Activity? = null
+    private var activity: Activity
     private var fragment: Fragment? = null
-    private var permissions: Array<String>? = null
+    private var permissions: Array<String> = arrayOf()
     private var mPermissionCallback: PermissionCallback? = null
     private var showRational: Boolean = false
 
     //=========Constructors - START=========
-    constructor(activity: Activity, fragment: Fragment, permissions: Array<String>, requestCode: Int) {
+    constructor(activity: Activity, fragment: Fragment, permissions: Array<String>, requestCode: Int = 100) {
         this.activity = activity
         this.fragment = fragment
         this.permissions = permissions
@@ -40,30 +38,21 @@ class PermissionHelper {
         checkIfPermissionPresentInAndroidManifest()
     }
 
-    constructor(fragment: Fragment, permissions: Array<String>, requestCode: Int = 100) {
-        this.fragment = fragment
-        this.permissions = permissions
-        this.REQUEST_CODE = requestCode
-        checkIfPermissionPresentInAndroidManifest()
-    }
-
     private fun checkIfPermissionPresentInAndroidManifest() {
-        for (permission in permissions!!) {
-            if (hasPermissionInManifest(permission) == false) {
+        for (permission in permissions) {
+            if (!hasPermissionInManifest(permission)) {
                 throw RuntimeException("Permission ($permission) Not Declared in manifest")
             }
         }
     }
 
+
     //=========Constructors- END=========
     fun request(permissionCallback: PermissionCallback?) {
         this.mPermissionCallback = permissionCallback
-        if (hasPermission() == false) {
-            showRational = shouldShowRational(permissions!!)
-            if (activity != null)
-                ActivityCompat.requestPermissions(activity!!, filterNotGrantedPermission(permissions!!), REQUEST_CODE)
-            else
-                fragment!!.requestPermissions(filterNotGrantedPermission(permissions!!), REQUEST_CODE)
+        if (!hasPermssion()) {
+            showRational = shouldShowRational(permissions)
+            ActivityCompat.requestPermissions(activity, filterNotGrantedPermission(permissions), REQUEST_CODE)
         } else {
             Log.i(TAG, "PERMISSION: Permission Granted")
             mPermissionCallback?.onPermissionGranted()
@@ -71,48 +60,44 @@ class PermissionHelper {
         }
     }
 
-    private var requestAllCallback: () -> Unit? = fun() {};
-    fun requestAll(callback: () -> Unit) {
-        this.requestAllCallback = callback;
+    private var requestAllCallback: () -> Unit? = fun() {}
+    private var deniedCallback: (isSystem: Boolean) -> Unit? = fun(isSystem: Boolean) {}
+    fun requestAll(requestAllCallback: () -> Unit,deniedCallback: (isSystem: Boolean) -> Unit) {
+        this.requestAllCallback = requestAllCallback
+        this.deniedCallback = deniedCallback
         request(null)
     }
 
-    private var requestIndividualCallback: (grantedPermission: Array<String>) -> Unit? = fun(grantedPermission: Array<String>) {};
-    fun requestIndividual(callback: (grantedPermission: Array<String>) -> Unit) {
-        this.requestIndividualCallback = callback;
+    private var requestIndividualCallback: (grantedPermission: Array<String>) -> Unit? = fun(grantedPermission: Array<String>) {}
+    fun requestIndividual(requestIndividualCallback: (grantedPermission: Array<String>) -> Unit,deniedCallback: (isSystem: Boolean) -> Unit) {
+        this.requestIndividualCallback = requestIndividualCallback
+        this.deniedCallback = deniedCallback
         request(null)
-    }
-
-    private var deniedCallback: (isSystem: Boolean) -> Unit? = fun(isSystem: Boolean) {};
-    fun denied(callback: (isSystem: Boolean) -> Unit) {
-        this.deniedCallback = callback;
     }
 
     fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
 
         if (requestCode == REQUEST_CODE) {
             var denied = false
-            var i = 0
             val grantedPermissions = ArrayList<String>()
-            for (grantResult in grantResults) {
+            for ((i, grantResult) in grantResults.withIndex()) {
                 if (grantResult != PackageManager.PERMISSION_GRANTED) {
                     denied = true
                 } else {
                     grantedPermissions.add(permissions[i])
                 }
-                i++
             }
 
             if (denied) {
                 val currentShowRational = shouldShowRational(permissions)
-                if (showRational == false && currentShowRational == false) {
+                if (!showRational && !currentShowRational) {
                     Log.d(TAG, "PERMISSION: Permission Denied By System")
                     mPermissionCallback?.onPermissionDeniedBySystem()
                     deniedCallback(true)
                 } else {
                     Log.i(TAG, "PERMISSION: Permission Denied")
                     //Checking if any single individual permission is granted then show user that permission
-                    if (!grantedPermissions.isEmpty()) {
+                    if (grantedPermissions.isNotEmpty()) {
 
                         mPermissionCallback?.onIndividualPermissionGranted(grantedPermissions.toTypedArray())
                         requestIndividualCallback(grantedPermissions.toTypedArray())
@@ -144,11 +129,6 @@ class PermissionHelper {
         fun onPermissionDeniedBySystem()
     }
 
-
-    private fun <T : Context> getContext(): T? {
-        return if (activity != null) activity as T? else fragment!!.context as T
-    }
-
     /**
      * Return list that is not granted and we need to ask for permission
      *
@@ -158,7 +138,7 @@ class PermissionHelper {
     private fun filterNotGrantedPermission(permissions: Array<String>): Array<String> {
         val notGrantedPermission = ArrayList<String>()
         for (permission in permissions) {
-            if (ContextCompat.checkSelfPermission(getContext()!!, permission) != PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(activity, permission) != PackageManager.PERMISSION_GRANTED) {
                 notGrantedPermission.add(permission)
             }
         }
@@ -167,13 +147,10 @@ class PermissionHelper {
 
     /**
      * Check permission is there or not
-     *
-     * @param permissions
-     * @return
      */
-    fun hasPermission(): Boolean {
-        for (permission in permissions!!) {
-            if (ContextCompat.checkSelfPermission(getContext()!!, permission) != PackageManager.PERMISSION_GRANTED) {
+    fun hasPermssion(): Boolean {
+        for (permission in permissions) {
+            if (ContextCompat.checkSelfPermission(activity, permission) != PackageManager.PERMISSION_GRANTED) {
                 return false
             }
         }
@@ -186,9 +163,9 @@ class PermissionHelper {
      * @param permissions
      * @return
      */
-    fun checkSelfPermission(permissions: Array<String>?): Boolean {
-        for (permission in permissions!!) {
-            if (ContextCompat.checkSelfPermission(getContext()!!, permission) != PackageManager.PERMISSION_GRANTED) {
+    fun checkSelfPermission(permissions: Array<String>): Boolean {
+        for (permission in permissions) {
+            if (ContextCompat.checkSelfPermission(activity, permission) != PackageManager.PERMISSION_GRANTED) {
                 return false
             }
         }
@@ -206,16 +183,9 @@ class PermissionHelper {
         var currentShowRational = false
         for (permission in permissions) {
 
-            if (activity != null) {
-                if (ActivityCompat.shouldShowRequestPermissionRationale(activity!!, permission) == true) {
-                    currentShowRational = true
-                    break
-                }
-            } else {
-                if (fragment!!.shouldShowRequestPermissionRationale(permission) == true) {
-                    currentShowRational = true
-                    break
-                }
+            if (ActivityCompat.shouldShowRequestPermissionRationale(activity, permission)) {
+                currentShowRational = true
+                break
             }
         }
         return currentShowRational
@@ -224,10 +194,10 @@ class PermissionHelper {
     //===================
     private fun hasPermissionInManifest(permission: String): Boolean {
         try {
-            val context = if (activity != null) activity else fragment!!.activity
-            val info = context?.getPackageManager()?.getPackageInfo(context.getPackageName(), PackageManager.GET_PERMISSIONS)
+            val context = activity
+            val info = context.packageManager?.getPackageInfo(context.packageName, PackageManager.GET_PERMISSIONS)
             if (info?.requestedPermissions != null) {
-                for (p in info?.requestedPermissions) {
+                for (p in info.requestedPermissions) {
                     if (p == permission) {
                         return true
                     }
@@ -244,16 +214,13 @@ class PermissionHelper {
      * Open current application detail activity so user can change permission manually.
      */
     fun openAppDetailsActivity() {
-        if (getContext<Context>() == null) {
-            return
-        }
         val i = Intent()
         i.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
         i.addCategory(Intent.CATEGORY_DEFAULT)
-        i.data = Uri.parse("package:" + getContext<Context>()!!.packageName)
+        i.data = Uri.parse("package:" + activity.packageName)
         i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
         i.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS)
-        getContext<Context>()!!.startActivity(i)
+        activity.startActivity(i)
     }
 }
